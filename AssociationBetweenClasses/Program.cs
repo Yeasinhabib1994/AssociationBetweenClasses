48cf3a16177fecce6c37eeb860925dddc22f6868
yeasin habib <yeasinhabib1994@gmail.com>
2018-09-21 00:23:38 +0600

    Add project files.

---
+++ AssociationBetweenClasses/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationBetweenClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new Logger();
+            var dbMigrator = new DbMigrator(logger);
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
+        }
+    }
+
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class DbMigrator
+    {
+        private readonly Logger _logger;
+
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.Log("we are migrating...");
+        }
+    }
+
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("we are installing the application");
+        }
+    }
+}
